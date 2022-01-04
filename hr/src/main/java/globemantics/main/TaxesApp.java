34925a3431cb7aa8a829c2232a123b65ac8fa620
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2022-01-04 14:32:30 +0100

    OCP: Main for tax calculation

---
+++ hr/src/main/java/globemantics/main/TaxesApp.java
@@ -0,0 +1,42 @@
+package globemantics.main;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+import globemantics.logging.ConsoleLogger;
+import globemantics.persistence.EmployeeFileSerializer;
+import globemantics.persistence.EmployeeRepository;
+import globemantics.personnel.Employee;
+import globemantics.taxes.TaxCalculator;
+
+public class TaxesApp {
+    public static void main(String[] args) {
+        // Create employees
+        ConsoleLogger consoleLogger = new ConsoleLogger();
+        EmployeeFileSerializer employeeFileSerializer = new EmployeeFileSerializer();
+        EmployeeRepository employeeRepository = new EmployeeRepository(employeeFileSerializer);
+
+        // Get Employees
+        List<Employee> employees = employeeRepository.findAll();
+
+        // Calculate taxes
+        Locale locale = new Locale("en", "US");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+
+        // Create tax calculator
+        TaxCalculator taxCalculator = new TaxCalculator();
+
+        double totalTaxes = 0;
+        for (Employee employee : employees) {
+            // Compute individual tax
+            double tax = taxCalculator.calculate(employee);
+            String formattedTax = currencyFormatter.format(tax);
+            consoleLogger.writeInfo(employee.getFullName() + " taxes: " + formattedTax);
+
+            // Add to company total taxes
+            totalTaxes += taxCalculator.calculate(employee);
+        }
+        consoleLogger.writeInfo("Total taxes = " + currencyFormatter.format(totalTaxes));
+    }
+}
