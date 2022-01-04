f4c790da771151cd65ca1f6421a618bb39b8b147
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2022-01-04 17:36:22 +0100

    LSP: Refactoring approveSLA Main

--- hr/src/main/java/globemantics/main/ApproveSLAApp.java
+++ hr/src/main/java/globemantics/main/ApproveSLAApp.java
@@ -1,21 +1,13 @@
 package globemantics.main;
 
-import globemantics.logging.ConsoleLogger;
-import globemantics.persistence.EmployeeFileSerializer;
-import globemantics.persistence.EmployeeRepository;
-import globemantics.personnel.Employee;
 import globemantics.personnel.ServiceLicenseAgreement;
 import globemantics.personnel.Subcontractor;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ApproveSLAApp {
     public static void main(String[] args) {
-        // Create dependencies
-        ConsoleLogger consoleLogger = new ConsoleLogger();
-        EmployeeFileSerializer employeeFileSerializer = new EmployeeFileSerializer();
-        EmployeeRepository repository = new EmployeeRepository(employeeFileSerializer);
-
         // Define SLA
         int minTimeOffPercent = 98;
         int maxResolutionDays = 2;
@@ -23,14 +15,21 @@ public class ApproveSLAApp {
                 minTimeOffPercent,
                 maxResolutionDays);
 
-        // Grab subcontractors
-        List<Employee> subcontractors = repository.findAll();
+        // Get collaborators from their own source
+        Subcontractor subcontractor1 = new Subcontractor(
+                "Rebekah Jackson",
+                "rebekah-jackson@abc.com",
+                3000,
+                15);
+        Subcontractor subcontractor2 = new Subcontractor(
+                "Harry Fitz",
+                "harryfitz@def.com",
+                3000, 15);
+        List<Subcontractor> collaborators = Arrays.asList(subcontractor1, subcontractor2);
 
-        for (Employee e : subcontractors) {
-            if (e instanceof Subcontractor) {
-                Subcontractor s = (Subcontractor) e;
-                s.approveSLA(companySla);
-            }
+        // Check SLA
+        for (Subcontractor s : collaborators) {
+            s.approveSLA(companySla);
         }
     }
 }
